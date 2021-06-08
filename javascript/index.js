6aa179c49d3e36dbf6bfe7eed4fa6de87cf049ea
gkapitein <84490555+gkapitein@users.noreply.github.com>
2021-06-08 16:16:15 +0200

    Add files via upload

---
+++ javascript/index.js
@@ -0,0 +1,30 @@
+let firstName = "Geert";
+let lastName = "Kapitein";
+console.log(firstName + lastName);
+
+// dit kan niet want if een gereserveerde variabele
+
+let getal1 = 4;
+let getal2 = 4;
+console.log(getal1 + getal2);
+
+let getal3 = "4";
+let getal4 = "4";
+console.log("4" + "4");
+
+let getal5 = 2;
+let getal6 = 4;
+let getal7 = 6;
+let getal8 = 8;
+let getal9 = 10;
+console.log(getal5 + getal6);
+console.log(getal9 - getal8);
+console.log(getal7 * getal8);
+console.log(getal9 / getal5);
+console.log(getal8 % getal5);
+
+let age = 48;
+console.log(typeof age)
+
+let newAge = "48";
+console.log(typeof newAge)
